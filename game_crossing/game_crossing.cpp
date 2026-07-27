b997eda8a0ea73130e37854e95c69973680435dc
khang <nguyengiakhang19122007@gmail.com>
2026-07-27 18:21:57 +0700

    feat: use borderless fullscreen at 1920x1080

--- game_crossing/game_crossing.cpp
+++ game_crossing/game_crossing.cpp
@@ -3,9 +3,17 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "Zombie Apocalypse");
+    sf::RenderWindow window(
+        sf::VideoMode({ 1920, 1080 }),
+        "Zombie Apocalypse",
+        sf::Style::None
+    );
+
+    window.setPosition({ 0, 0 });
     window.setFramerateLimit(60);
+
     UIManager ui(window);
     ui.run();
+
     return 0;
 }

78840db2fe977bfb270f3b914d2d6fd34bd1a741
giahuy1001 <trangiahuy10012007@gmail.com>
2026-07-30 14:33:03 +0700

    Tuned vehicle's sizes

--- game_crossing/src/CCar.cpp
+++ game_crossing/src/CCar.cpp
@@ -14,7 +14,7 @@ static sf::Texture& getCarTexture(direction dir) {
 }
 
 CCar::CCar(float startX, float startY, direction dir)
-    : CVehicle(startX, startY, 80.0f, 50.0f, 350.0f, dir)
+    : CVehicle(startX, startY, 180.0f, 160.0f, 350.0f, dir)
     //x, y, width, height, speed, direction
 {
 
